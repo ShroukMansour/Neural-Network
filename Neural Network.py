c9d04c0878a1b930d01d4aaf80d974393c0c6063
ShroukMansour <shroukmansour99@gmail.com>
2018-12-10 22:28:07 +0200

    Activation func added

--- Neural Network.py
+++ Neural Network.py
@@ -6,14 +6,21 @@ class NeuralNetwork:
     def __init__(self, m, l, n):
         self.hidden_weights = np.random.rand(m, l)
         self.output_weights = np.random.rand(l, n)
+        self.hidden_layer_values = None
 
     def feed_forward(self, x_train):
-        hidden_layer_values = np.dot(x_train, self.hidden_weights) # (k*m).(m*L) = k*L
-        output = np.dot(hidden_layer_values, self.output_weights) # (k*L).(L*n) = k*n
+        hidden_layer_values = np.dot(x_train, self.hidden_weights)  # (k*m).(m*L) = k*L
+        self.activation_funcv = np.vectorize(self.activation_func)  # to apply on each elem
+        hidden_layer_values = self.activation_funcv(hidden_layer_values)
+        self.hidden_layer_values = np.array(hidden_layer_values)
+        output = np.dot(hidden_layer_values, self.output_weights)  # (k*L).(L*n) = k*n
         return output
 
     def feed_backward(self, x_train, y_train):
         pass
+
+    def activation_func(self, net):
+        return 1 / (1 + np.math.exp(-net))
 
     def calc_mse(self, yt, yp):
         return sum((yt - yp)**2)
@@ -25,7 +32,7 @@ l = info[1]  # hidden neurons
 n = info[2]  # classes
 k = int(input())  # num of samples
 x_train = []  # k * m
-y_train = [] #  k * n
+y_train = []  #  k * n
 for i in range(k):
     sample = [float(x) for x in input().split()]
     x_train.append(np.array(sample[0:m]))
